c5bca5a2c6f9c4aa8020d4b77a2a3a289bea574f
Ivo Verburgh <Ivo.Verburgh@thelearningnetwork.nl>
2023-06-20 20:47:53 +0200

    Initial commit

---
+++ Calculator-StrykerNETExample/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculator_StrykerNETExample
+{
+    public class Calculator
+    {
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        public int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        public (int Result, int Remainder) Divide(int value1, int value2)
+        {
+            if (value2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            var result = value1 / value2;
+            var remainder = result % value2;
+            return (result, remainder);
+        }
+    }
+}
