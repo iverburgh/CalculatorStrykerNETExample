2c43e4b23af74599790cce9a93dbf3ffc9c69fb9
Ivo Verburgh <I.Verburgh@4dotnet.nl>
2024-06-20 14:16:33 +0200

    Updated project

--- Calculator-StrykerNETExample/Calculator.cs
+++ Calculator-StrykerNETExample/Calculator.cs
@@ -1,31 +1,30 @@
-﻿namespace Calculator_StrykerNETExample
+﻿namespace Calculator_StrykerNETExample;
+
+public class Calculator
 {
-    public class Calculator
+    public int Add(int value1, int value2)
     {
-        public int Add(int value1, int value2)
-        {
-            return value1 + value2;
-        }
+        return value1 + value2;
+    }
 
-        public int Subtract(int value1, int value2)
-        {
-            return value1 - value2;
-        }
+    public int Subtract(int value1, int value2)
+    {
+        return value1 - value2;
+    }
 
-        public int Multiply(int value1, int value2)
-        {
-            return value1 * value2;
-        }
+    public int Multiply(int value1, int value2)
+    {
+        return value1 * value2;
+    }
 
-        public (int Result, int Remainder) Divide(int value1, int value2)
+    public (int Result, int Remainder) Divide(int value1, int value2)
+    {
+        if (value2 == 0)
         {
-            if (value2 == 0)
-            {
-                throw new DivideByZeroException();
-            }
-            var result = value1 / value2;
-            var remainder = result % value2;
-            return (result, remainder);
+            throw new DivideByZeroException();
         }
+        var result = value1 / value2;
+        var remainder = result % value2;
+        return (result, remainder);
     }
 }
--- Calculator-StrykerNETExampleTests/CalculatorTests.cs
+++ Calculator-StrykerNETExampleTests/CalculatorTests.cs
@@ -1,81 +1,75 @@
 using Calculator_StrykerNETExample;
 using FluentAssertions;
 
-namespace Calculator_StrykerNETExampleTests
+namespace Calculator_StrykerNETExampleTests;
+
+public class CalculatorTests
 {
-    public class CalculatorTests
+    private readonly Calculator _subject = new();
+
+    [Theory]
+    [InlineData(5, 5, 10)]
+    public void Add_WhenValuesAreAdded_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult)
     {
-        private readonly Calculator _subject;
-
-        public CalculatorTests()
-        {
-            _subject = new Calculator();
-        }
-
-        [Theory]
-        [InlineData(5, 5, 10)]
-        public void Add_WhenValuesAreAdded_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult)
-        {
-            // arrange
+        // arrange
             
-            // act
-            var result = _subject.Add(value1, value2);
+        // act
+        var result = _subject.Add(value1, value2);
             
-            // assert
-            result.Should().Be(expectedResult);
-        }
+        // assert
+        result.Should().Be(expectedResult);
+    }
 
-        [Theory]
-        [InlineData(5, 5, 0)]
-        public void Subtract_WhenValuesAreSubtracted_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult)
-        {
-            // arrange
+    [Theory]
+    [InlineData(5, 5, 0)]
+    public void Subtract_WhenValuesAreSubtracted_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult)
+    {
+        // arrange
 
-            // act
-            var result = _subject.Subtract(value1, value2);
+        // act
+        var result = _subject.Subtract(value1, value2);
 
-            // assert
-            result.Should().Be(expectedResult);
-        }
+        // assert
+        result.Should().Be(expectedResult);
+    }
 
-        [Theory]
-        [InlineData(1, 1, 1)]
-        public void Multiply_WhenValuesAreMultiplied_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult)
-        {
-            // arrange
+    [Theory]
+    [InlineData(1, 1, 1)]
+    public void Multiply_WhenValuesAreMultiplied_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult)
+    {
+        // arrange
 
-            // act
-            var result = _subject.Multiply(value1, value2);
+        // act
+        var result = _subject.Multiply(value1, value2);
 
-            // assert
-            result.Should().Be(expectedResult);
-        }
+        // assert
+        result.Should().Be(expectedResult);
+    }
 
-        [Theory]
-        [InlineData(1, 1, 1, 0)]
-        public void Divide_WhenValuesAreDivided_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult, int expectedRemainder)
-        {
-            // arrange
+    [Theory]
+    [InlineData(1, 1, 1, 0)]
+    public void Divide_WhenValuesAreDivided_ThenCorrectResponseIsReturned(int value1, int value2, int expectedResult, int expectedRemainder)
+    {
+        // arrange
 
-            // act
-            var (result, remainder) = _subject.Divide(value1, value2);
+        // act
+        var (result, remainder) = _subject.Divide(value1, value2);
 
-            // assert
-            result.Should().Be(expectedResult);
-            remainder.Should().Be(expectedRemainder);
-        }
+        // assert
+        result.Should().Be(expectedResult);
+        remainder.Should().Be(expectedRemainder);
+    }
 
-        [Fact]
-        public void Divide_WhenDividedByZero_ThenExceptionIsThrown()
-        {
-            // arrange
+    [Fact]
+    public void Divide_WhenDividedByZero_ThenExceptionIsThrown()
+    {
+        // arrange
 
-            // act
-            _subject
-                .Invoking(s => s.Divide(1, 0))
-                .Should().Throw<DivideByZeroException>();
+        // act
+        _subject
+            .Invoking(s => s.Divide(1, 0))
+            .Should().Throw<DivideByZeroException>();
 
-            // assert
-        }
+        // assert
     }
 }
